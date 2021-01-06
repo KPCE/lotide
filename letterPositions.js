03cb198114f2998f29008c360fceaf89cd1be4cd
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-06 22:22:33 +0000

    adding function that returns an object with the indices of each letter

---
+++ letterPositions.js
@@ -0,0 +1,17 @@
+//take in sentance, return object whose keys are the letters (without spaces)
+//and whose values are arrays comprised of the indices where each letter can be found in the sentence passed
+const letterPositions = function(sentence) {
+  let results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+      //results[letter] += 1; //change to return indice not count
+    } else if (sentence[i] === ' ') {
+    } else {
+      results[sentence[i]] = [i]; //change to return array of the indice letter
+    }
+  }
+  
+  return results;
+};
+console.log(letterPositions);
