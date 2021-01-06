0906eeaf3ef6482678f3bdd6521c2ef3ae8ce08b
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-06 01:36:35 +0000

    adding array comparison function

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+// assert equal function to compare two inputs
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+//function to take in two arrays and return true or false based on perfect match
+
+let eqArrays = function(arr1, arr2){
+  if (arr1 === arr2){
+    return true
+  } else {
+    return false
+  }
+}
+
+
+//test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
