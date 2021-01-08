3c910e00e637e96947dc89a2411e1bab0941ea42
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-08 01:59:37 +0000

    adding a find key given a callback function

---
+++ findKey.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//function should return the key of the first value to match the callback
+//loop through the objects, if statement to compare callback against value
+//if match, return the key at that position
+const findKey = function(object, callback){
+  let output = undefined
+  for (let i in object) {
+
+    if (callback(object[i])) {
+      output = i
+      break
+    }
+ 
+  }
+  return output
+}
+
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri"); 
