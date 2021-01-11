d8bae90e9d914290f504c28de12b2594f81b91a5
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-11 18:30:53 +0000

    updated middle to use module/export and moved tests

--- middle.js
+++ middle.js
@@ -1,28 +1,4 @@
-//function to compare two arrays, returns true if identical or false otherwise
-let eqArrays = function(arr1, arr2){
-  let bool = false;
-  if (arr1.length === arr2.length){
-    bool = true;
-    for (let x = 0; x < arr1.length; x++){
-      if (arr1[x] === arr2[x]){
-        bool = true;
-      } else {
-        return false;
-      }
-    }
-  }
-  return bool;
-};
-
-// function that returns the assertion of truthness or falseness
-let assertArraysEqual = function(arr1, arr2){
-  if(eqArrays(arr1, arr2)){
-    return `this is true`
-  } else {
-    return `this is false`
-  }
-}
-
+const assertArraysEqual = require('./assertArraysEqual');
 
 //function middle to return an array of the middle value{s}
 //check if one or two elements => return empty array
@@ -45,8 +21,5 @@ let middle = function (arr) {
 }
 
 
-//test cases
-console.log(assertArraysEqual(middle([1, 2, 3]),[2])) // => [2]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])) // => [3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])) // => [2, 3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])) // => [3, 4]
+
+module.exports = middle;
