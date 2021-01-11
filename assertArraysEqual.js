c6924c0d7234e5c17226c3eaa79720d5dbb650fb
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-11 17:20:43 +0000

    assert arrays equal updated with export/require and removed tests

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,27 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-//function to compare two arrays, returns true if identical or false otherwise
-let eqArrays = function(arr1, arr2){
-  let bool = false;
-  if (arr1.length === arr2.length){
-    bool = true;
-    for (let x = 0; x < arr1.length; x++){
-      if (arr1[x] === arr2[x]){
-        bool = true;
-      } else {
-        return false;
-      }
-    }
-  }
-  return bool;
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 // function that returns the assertion of truthness or falseness
 let assertArraysEqual = function(arr1, arr2){
@@ -32,11 +10,4 @@ let assertArraysEqual = function(arr1, arr2){
   }
 }
 
-
-
-//test cases
-assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]), false) // => false
-
-assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]), false) // => false
+module.exports = assertArraysEqual;
